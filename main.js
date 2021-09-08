4c944617c6ac95ef16d8d4b5517a398bb1ad2525
hrithikM31 <40201847+hrithikM31@users.noreply.github.com>
2021-09-08 11:39:23 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == "") c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+
+if (window.location.href  == "http://www.rabphaltt.bt/book_now"){
+	var item = readCookie("Item");
+  	document.cookie = "Item=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  	document.getELementById("nf-field-15").value = item;
+}
